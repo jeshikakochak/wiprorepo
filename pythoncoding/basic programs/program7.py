d31956d6d2a810fd0aee6118aaeb5c72711da429
jeshikakochak <jeshikakochak@gmail.com>
2026-04-23 12:26:01 +0530

    practice

---
+++ pythoncoding/basic programs/program7.py
@@ -0,0 +1,9 @@
+num=int(input("enter how many* "))
+for _ in range(1,num+1):
+    print('*',sep='',end='______')
+
+#---------------------------------------
+
+num=int(input("\nenter how many*"))
+for _ in range(1,num+1):
+    print('*','&',sep='%%%%',end='----')
